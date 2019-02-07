e20de9959aec8083cf28f4ed166185557dd1d288
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-02-07 22:13:03 +0100

    Added <numeric> header to default includes

--- source/cppx-core/core-language/calc/std-calc.hpp
+++ source/cppx-core/core-language/calc/std-calc.hpp
@@ -2,4 +2,5 @@
 
 #include <c/math.hpp>       // <math.h>, <cmath>, <c/stdlib.hpp>
 #include <complex>          // std::complex
+#include <numeric>          // std::accumulate etc.
 // Intentionally not including <valarray> because it doesn't follow std library conventions.
--- source/cppx-core/stdlib-includes/basic-general.hpp
+++ source/cppx-core/stdlib-includes/basic-general.hpp
@@ -9,20 +9,22 @@
 #include <c/string.hpp>     // strlen, ++
 #include <c/wchar.hpp>      // wcslen, ++
 #include <c/wctype.hpp>     // - Wide text classifiers.
+
 #include <algorithm>        // std::(min, max, ...)
 #include <array>            // std::(array)
+#include <atomic>           // - atomic operations support for threading
 #include <chrono>           // std::chrono::*
 #include <deque>            // std::deque (e.g. default container for std::stack)
 #include <functional>       // std::(function)
 #include <iosfwd>           // - Forward declarations of stream types etc.
 #include <iterator>         // std::(begin, end, size)
+#include <numeric>          // std::accumulate etc.
 #include <queue>            // std::queue
 #include <random>           // std::(...)
 #include <stack>            // std::stack
 #include <stdexcept>        // std::(...). But see <@/failure-handling/macro_fail.hpp>.
 #include <string>           // std::string
 #include <string_view>      // - But see <@/text/String_view_.hpp>.
-#include <atomic>           // - atomic operations support for threading
 #include <thread>           // std::thread, std::this_thread::sleep_for
 #include <unordered_map>    // - But see <@/collections/Map_.hpp>
 #include <unordered_set>    // - But see <@/collections/Set_.hpp>
