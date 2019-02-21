1d08e6a1b4ecbf40ad9ce0d5c21e68ff73c25a48
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-02-21 06:12:49 +0100

    readability

--- source/cppx-core/config.hpp
+++ source/cppx-core/config.hpp
@@ -2,9 +2,10 @@
 
 namespace cppx
 {
-    // Header <cppx-core/text/ascii/ascii-fallback-symbols.hpp>
     constexpr bool  check_the_execution_character_set       = true;
+    // Header <cppx-core/text/ascii/ascii-fallback-symbols.hpp>
 
-    // Header <cppx-core/text/unicode/assert-utf8-execution-character-set.hpp>
     constexpr bool  use_ascii_substitutes                   = false;
+    // Header <cppx-core/text/unicode/assert-utf8-execution-character-set.hpp>
+
 }  // namespace cppx
