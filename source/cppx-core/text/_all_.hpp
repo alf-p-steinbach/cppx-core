a9df329fce3a2f33c332a0fc913cbe68a640dcdb
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-04-05 18:42:11 +0200

    basic string building (not efficient, but convenient)

--- source/cppx-core/text/_all_.hpp
+++ source/cppx-core/text/_all_.hpp
@@ -3,5 +3,6 @@
 #include <cppx-core/text/ascii/_all_.hpp>
 #include <cppx-core/text/unicode/_all_.hpp>
 
+#include <cppx-core/text/basic-string-building.hpp>
 #include <cppx-core/text/pointers-from-string_view.hpp>
 #include <cppx-core/text/string-util.hpp>
---
+++ source/cppx-core/text/basic-string-building.hpp
@@ -0,0 +1,29 @@
+﻿#pragma once    // Source encoding: UTF-8 with BOM (π is a lowercase Greek "pi").
+
+#include <string>           // std::string
+#include <sstream>          // std::ostringstream
+
+namespace cppx
+{
+    namespace basic_string_building
+    {
+        using std::string, std::ostringstream;
+        using namespace std::string_literals;       // E.g. ""s
+
+        template< class Type >
+        inline auto operator<<( string& s, Type const& value )
+            -> string&
+        {
+            ostringstream stream;
+            stream << value;
+            s += stream.str();
+            return s;
+        }
+
+        template< class Type >
+        inline auto operator<<( string&& s, Type const& value )
+            -> string&&
+        { return  move( operator<<( s, value ) ); }
+
+    }  // namespace basic_string_building
+}  // namespace cppx
