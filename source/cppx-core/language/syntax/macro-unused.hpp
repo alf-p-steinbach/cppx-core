351da16cf02439f3e7cae9210c617cd1ffbd9367
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-04-19 06:04:59 +0200

    doc

--- source/cppx-core/language/syntax/macro-unused.hpp
+++ source/cppx-core/language/syntax/macro-unused.hpp
@@ -1,7 +1,12 @@
 ﻿#pragma once    // Source encoding: UTF-8 with BOM (π is a lowercase Greek "pi").
+/// \file
+/// \brief `$unused` expands to C++17 `[[maybe_unused]]`.
 
 #ifndef CPPX_NO_DOLLARS_PLEASE
-#   define  $unused     CPPX_UNUSED
+#   define  $unused     CPPX_UNUSED                 ///< \dollarname{unused}
 #endif
 
-#define CPPX_UNUSED     [[maybe_unused]]            // C++17 wordy attribute
+#ifndef CPPX_UNUSED
+/// \brief Expands to C++17 `[[maybe_unused]]`.
+#   define  CPPX_UNUSED     [[maybe_unused]]        // C++17 wordy/misleading attribute.
+#endif
--- source/cppx-core/language/syntax/macro-use.hpp
+++ source/cppx-core/language/syntax/macro-use.hpp
@@ -1,50 +1,88 @@
 ﻿#pragma once    // Source encoding: UTF-8 with BOM (π is a lowercase Greek "pi").
+/// \file
+/// \brief Macros for generating more concise and clear `using` statements.
 
 #include <cppx-core/meta-macro/macro-apply_with_fixed_arg.hpp>
 
 #ifndef CPPX_NO_DOLLARS_PLEASE
-#   define $use_from_namespace          CPPX_USE_FROM_NAMESPACE
-#   define $use_cppx                    CPPX_USE_CPPX
-#   define $use_std                     CPPX_USE_STD
-#   define $use_namespace_name_in       CPPX_USE_NAMESPACE_NAME_IN
-#   define $use_cppx_namespace_name     CPPX_USE_CPPX_NAMESPACE_NAME
-#   define $use_std_namespace_name      CPPX_USE_STD_NAMESPACE_NAME
-#   define $use_namespace_names_in      CPPX_USE_NAMESPACE_NAMES_IN
-#   define $use_cppx_namespace_names    CPPX_USE_CPPX_NAMESPACE_NAMES
-#   define $use_std_namespace_names     CPPX_USE_STD_NAMESPACE_NAMES
+#   define $use_from_namespace          CPPX_USE_FROM_NAMESPACE         ///< \dollarname{use_from_namespace}
+#   define $use_cppx                    CPPX_USE_CPPX                   ///< \dollarname{use_cppx}
+#   define $use_std                     CPPX_USE_STD                    ///< \dollarname{use_std}
+#   define $use_namespace_name_in       CPPX_USE_NAMESPACE_NAME_IN      ///< \dollarname{use_namespace_name_in}
+#   define $use_cppx_namespace_name     CPPX_USE_CPPX_NAMESPACE_NAME    ///< \dollarname{use_cppx_namespace_name}
+#   define $use_std_namespace_name      CPPX_USE_STD_NAMESPACE_NAME     ///< \dollarname{use_std_namespace_name}
+#   define $use_namespace_names_in      CPPX_USE_NAMESPACE_NAMES_IN     ///< \dollarname{use_namespace_names_in}
+#   define $use_cppx_namespace_names    CPPX_USE_CPPX_NAMESPACE_NAMES   ///< \dollarname{use_cppx_namespace_names}
+#   define $use_std_namespace_names     CPPX_USE_STD_NAMESPACE_NAMES    ///< \dollarname{use_std_namespace_names}
 #endif
 
+/// \hideinitializer
+/// \brief Use each specified item `ITEM` from namespace `NS` as if by `using NS::ITEM;`.
+///
+/// \param  ns      The namespace name.
+/// \param  ...     A comma separated list of items to use unqualified.
 #define CPPX_USE_FROM_NAMESPACE( ns, ... ) \
     CPPX_APPLY_WITH_FIXED_ARG( CPPX_GENERATE_USING_DECLARATION_FOR, ns, __VA_ARGS__ ) \
     static_assert( !!"Terminate this statement with a semicolon" )
 
+/// @cond DOXY_SHOW_IMPL_DETAILS
 #define CPPX_GENERATE_USING_DECLARATION_FOR( ns, name ) \
     using ns::name;
+/// @endcond
 
+/// \brief Use each specified item `ITEM` from namespace `cppx` as if by `using cppx::ITEM;`.
+///
+/// \param  ...     A comma separated list of items to use unqualified.
 #define CPPX_USE_CPPX( ... ) \
     CPPX_USE_FROM_NAMESPACE( cppx, __VA_ARGS__ )
 
+/// \brief Use each specified item `ITEM` from namespace `std` as if by `using std::ITEM;`.
+///
+/// \param  ...     A comma separated list of items to use unqualified.
 #define CPPX_USE_STD( ... ) \
     CPPX_USE_FROM_NAMESPACE( std, __VA_ARGS__ )
 
+/// \brief Use `NESTED_NS` as an alias for namespace `PARENT_NS::NESTED_NS`.
+///
+/// \param  parent_ns   The namespace containing the relevant nested namespace.
+/// \param  nested_ns   The nested namespace.
 #define CPPX_USE_NAMESPACE_NAME_IN( parent_ns, nested_ns ) \
     namespace nested_ns = parent_ns::nested_ns
 
-#define CPPX_USE_CPPX_NAMESPACE_NAME( name ) \
-    CPPX_USE_NAMESPACE_NAME_IN( cppx, name )
+/// \brief Use `NESTED_NS` as an alias for namespace `cppx::NESTED_NS`.
+///
+/// \param  nested_ns   The nested namespace.
+#define CPPX_USE_CPPX_NAMESPACE_NAME( nested_ns ) \
+    CPPX_USE_NAMESPACE_NAME_IN( cppx, nested_ns )
 
-#define CPPX_USE_STD_NAMESPACE_NAME( name ) \
-    CPPX_USE_NAMESPACE_NAME_IN( std, name )
+/// \brief Use `NESTED_NS` as an alias for namespace `std::NESTED_NS`.
+///
+/// \param  nested_ns   The nested namespace.
+#define CPPX_USE_STD_NAMESPACE_NAME( nested_ns ) \
+    CPPX_USE_NAMESPACE_NAME_IN( std, nested_ns )
 
+/// \hideinitializer
+/// \brief Use each specified `NESTED_NS` as an alias for namespace `PARENT_NS::NESTED_NS`.
+///
+/// \param  parent_ns   The namespace containing the relevant nested namespace.
+/// \param  ...         A comma separated list of names of nested namespaces.
 #define CPPX_USE_NAMESPACE_NAMES_IN( parent_ns, ... ) \
     CPPX_APPLY_WITH_FIXED_ARG( CPPX_GENERATE_NAMESPACE_NAME_USAGE, parent_ns, __VA_ARGS__ ) \
     static_assert( !!"Terminate this statement with a semicolon" )
 
+/// @cond DOXY_SHOW_IMPL_DETAILS
 #define CPPX_GENERATE_NAMESPACE_NAME_USAGE( parent_ns, nested_ns ) \
     namespace nested_ns = parent_ns::nested_ns;
+/// @endcond
 
+/// \brief Use each specified `NESTED_NS` as an alias for namespace `cppx::NESTED_NS`.
+///
+/// \param  ...         A comma separated list of names of nested namespaces.
 #define CPPX_USE_CPPX_NAMESPACE_NAMES( ... ) \
     CPPX_USE_NAMESPACE_NAMES_IN( cppx, __VA_ARGS__ )
 
+/// \brief Use each specified `NESTED_NS` as an alias for namespace `std::NESTED_NS`.
+///
+/// \param  ...         A comma separated list of names of nested namespaces.
 #define CPPX_USE_STD_NAMESPACE_NAMES( ... ) \
     CPPX_USE_NAMESPACE_NAMES_IN( std, __VA_ARGS__ )
