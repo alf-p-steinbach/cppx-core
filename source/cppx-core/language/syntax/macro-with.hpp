b123d4d78287f9f7f55778343f6cfe08227709cf
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-04-19 09:02:01 +0200

    -

--- source/cppx-core/language/syntax/macro-with.hpp
+++ source/cppx-core/language/syntax/macro-with.hpp
@@ -1,21 +1,29 @@
 ﻿#pragma once    // Source encoding: UTF-8 with BOM (π is a lowercase Greek "pi").
+/// \file
+/// \brief `$with` binds the specified declarator to `auto&& _` in the following braces block.
 
-// For example
-// $with( Lock( mutex ) ) access_resource();
-// $with_const( Lock( mutex ) ) access_resource();
-// $with( cppx::Const_<Lock>( mutex ) ) access_resource();
 
 #ifndef CPPX_NO_DOLLARS_PLEASE
-#   define $with        CPPX_WITH
-#   define $with_const  CPPX_WITH_CONST
+#   define $with        CPPX_WITH               ///< \dollarname{with}
+#   define $with_const  CPPX_WITH_CONST         ///< \dollarname{with_const}
 #endif
 
 #ifdef _MSC_VER
 #   pragma warning( disable: 4127 )     // Sillywarning "conditional expression is constant".
 #endif
 
+/// \brief Binds the specified declarator to `auto&& _` in the following braces block.
+///
+/// For example:  
+/// `$with( Lock( mutex ) ) access_resource();`  
+/// `$with( cppx::Const_<Lock>( mutex ) ) access_resource();`
+
 #define CPPX_WITH( ... ) \
     if ( auto&& _ = __VA_ARGS__; true or !!&_ )     // The !!&_ avoids warning about unused.
 
+/// \brief Binds the specified declarator to `const auto&& _` in the following braces block.
+///
+/// For example:  
+/// `$with_const( Lock( mutex ) ) access_resource();`
 #define CPPX_WITH_CONST( ... ) \
     if( const auto& _ = __VA_ARGS__; true or !!&_ )
