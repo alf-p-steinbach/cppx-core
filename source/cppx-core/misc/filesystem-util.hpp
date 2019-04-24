890d9e20ebf2c7037432b9187c2b222f5a12286c
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-04-24 05:10:05 +0200

    -

--- source/cppx-core/misc/filesystem-util.hpp
+++ source/cppx-core/misc/filesystem-util.hpp
@@ -17,79 +17,92 @@
 
 CPPX_DEFINE_TAG( Read );
 CPPX_DEFINE_TAG( Write );
+CPPX_DEFINE_TAG( Append );
 namespace cppx
 {
     namespace fs = std::filesystem;
     CPPX_USE_STD( copy, exchange, string );
 
-    template<
-        class Fs_path,
-        class = Enable_if_<is_same_type_<Fs_path, fs::path>>
-    >
-        auto utf8_from( const Fs_path& path )
-        -> string
+    inline namespace fs_util
     {
-        if constexpr( is_same_type_<string, decltype( path.u8string() )> )
+        template<
+            class Fs_path,
+            class = Enable_if_<is_same_type_<Fs_path, fs::path>>
+        >
+            auto utf8_from( const Fs_path& path )
+            -> string
         {
-            return path.u8string();
+            if constexpr( is_same_type_<string, decltype( path.u8string() )> )
+            {
+                return path.u8string();
+            }
+            else
+            {
+                $with( path.u8string() ) { return string( _.begin(), _.end() ); }
+            }
         }
-        else
-        {
-            $with( path.u8string() ) { return string( _.begin(), _.end() ); }
-        }
-    }
 
-    inline auto open_c_file( const fs::path& path, const C_str options )
-        -> P_<FILE>
-    {
-        #ifdef _WIN32
-            using std::wstring;
-            const auto woptions = wstring( options, options + strlen( options ) );
-            return _wfopen( path.wstring().c_str(), woptions.c_str() );
-        #else
-            return fopen( path.string().c_str(), options );
-        #endif
-    }
-
-    class C_file
-    {
-        C_file( const C_file& ) = delete;
-        auto operator=( const C_file& ) -> C_file& = delete;
-
-        P_<FILE>    m_file;
-
-    public:
-        auto file_pointer() const
+        inline auto open_c_file( const fs::path& path, const C_str options )
             -> P_<FILE>
-        { return m_file; }
+        {
+            #ifdef _WIN32
+                using std::wstring;
+                const auto woptions = wstring( options, options + strlen( options ) );
+                return _wfopen( path.wstring().c_str(), woptions.c_str() );
+            #else
+                return fopen( path.string().c_str(), options );
+            #endif
+        }
+
+        class C_file
+        {
+            C_file( const C_file& ) = delete;
+            auto operator=( const C_file& ) -> C_file& = delete;
+
+            P_<FILE>    m_file;
+
+        public:
+            auto file_pointer() const
+                -> P_<FILE>
+            { return m_file; }
         
-        operator P_<FILE> () const { return file_pointer(); }
+            operator P_<FILE> () const { return file_pointer(); }
 
-        auto release()
-            -> P_<FILE>
-        { return exchange( m_file, nullptr ); }
+            auto release()
+                -> P_<FILE>
+            { return exchange( m_file, nullptr ); }
 
-        ~C_file()
-        {
-            if( m_file ) { fclose( m_file ); }
-        }
+            ~C_file()
+            {
+                if( m_file ) { fclose( m_file ); }
+            }
 
-        C_file( tag::Read, const fs::path& path ):
-            m_file( open_c_file( path, "rb" ) )
-        {
-            hopefully( m_file != nullptr )
-                or CPPX_FAIL(
-                    "Unable to open "s << quoted( cppx::utf8_from( path ) ) << " for reading"
-                );
-        }
+            C_file( tag::Read, const fs::path& path ):
+                m_file( open_c_file( path, "rb" ) )
+            {
+                hopefully( m_file != nullptr )
+                    or CPPX_FAIL(
+                        "Unable to open "s << quoted( cppx::utf8_from( path ) ) << " for reading"
+                    );
+            }
 
-        C_file( tag::Write, const fs::path& path ):
-            m_file( open_c_file( path, "wb" ) )
-        {
-            hopefully( m_file != nullptr )
-                or CPPX_FAIL(
-                    "Unable to open "s << quoted( cppx::utf8_from( path ) ) << " for writing"
-                );
-        }
-    };
+            C_file( tag::Write, const fs::path& path ):
+                m_file( open_c_file( path, "wb" ) )
+            {
+                hopefully( m_file != nullptr )
+                    or CPPX_FAIL(
+                        "Unable to open "s << quoted( cppx::utf8_from( path ) ) << " for writing"
+                    );
+            }
+
+            C_file( tag::Append, const fs::path& path ):
+                m_file( open_c_file( path, "wab" ) )
+            {
+                hopefully( m_file != nullptr )
+                    or CPPX_FAIL(
+                        "Unable to open "s << quoted( cppx::utf8_from( path ) ) << " for appending"
+                    );
+            }
+        };
+    }  // namespace fs_util
 }  // cppx
