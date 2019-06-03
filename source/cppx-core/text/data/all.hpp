68b75613bfbc7c9680da951dc6756802b185a7f9
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-06-03 04:46:29 +0200

    hex digits data

--- source/cppx-core/text/data/all.hpp
+++ source/cppx-core/text/data/all.hpp
@@ -1,4 +1,5 @@
 ﻿#pragma once    // Source encoding: UTF-8 with BOM (π is a lowercase Greek "pi").
 
 #include <cppx-core/text/data/ascii-character-names.hpp>
+#include <cppx-core/text/data/hex-digits.hpp>
 #include <cppx-core/text/data/Symbol_strings.hpp>
---
+++ source/cppx-core/text/data/hex-digits.hpp
@@ -0,0 +1,13 @@
+﻿#pragma once    // Source encoding: UTF-8 with BOM (π is a lowercase Greek "pi").
+
+#include <cppx-core/language/syntax/macro-use.hpp>          // CPPX_USE_STD
+#include <cppx-core/language/calc/integer-operations.hpp>   // cppx::div_up
+#include <cppx-core/language/system/Byte.hpp>               // cppx::bits_per_byte
+
+namespace cppx {
+
+    constexpr char  hex_digits_uppercase[]  = "0123456789ABCDEF";
+    constexpr char  hex_digits_lowercase[]  = "0123456789abcdef";
+    constexpr int   hex_digits_per_byte     = div_up( bits_per_byte, 4 );
+
+}  // namespace cppx
