631be2d46f96384bd5e012cdf4441f0b573068a6
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-06-18 12:04:05 +0200

    `bytes_per_` + byte pointer conversion functions

--- source/cppx-core/language/system/Byte.hpp
+++ source/cppx-core/language/system/Byte.hpp
@@ -2,7 +2,9 @@
 /// \file
 /// \brief
 /// \make_name_ref{cppx,Byte},
-/// \make_name_ref{cppx,Signed_byte} and
+/// \make_name_ref{cppx,Signed_byte},
+/// \make_name_ref{cppx,as_byte_ptr},
+/// \make_name_ref{cppx,as_signedbyte_ptr} and
 /// \make_name_ref{cppx,bits_per_byte}, + `std::byte` support definitions
 /// \make_name_ref{cppx,as_number} and
 /// \make_name_ref{cppx,as_std_byte}.
@@ -11,20 +13,57 @@
 /// defines the general bit size function template `bits_per_byte`.
 
 #include <cppx-core/language/syntax/macro-use.hpp>          // CPPX_USE_STD
+#include <cppx-core/language/system/size-types.hpp>         // cppx::Size
+#include <cppx-core/language/syntax/type-assemblers.hpp>    // cppx::P_
 
 #include <c/limits.hpp>     // CHAR_BIT
 #include <c/stddef.hpp>     // std::byte
 
 namespace cppx
 {
-    /// Default choice of byte type.
+    /// \brief Default choice of byte type.
     using Byte              = unsigned char;
     using Signed_byte       = signed char;
+
+    inline auto as_byte_ptr( const P_<Signed_byte> p ) -> P_<Byte>;
+    inline auto as_byte_ptr( const P_<const Signed_byte> p ) -> P_<const Byte>;
+    inline auto as_signedbyte_ptr( const P_<Byte> p ) -> P_<Signed_byte>;
+    inline auto as_signedbyte_ptr( const P_<const Byte> p ) -> P_<const Signed_byte>;
 
     /// \brief Usually 8, but e.g. 16 on some DSPs.
     constexpr int bits_per_byte = CHAR_BIT;
 
+    /// \brief The number of bytes per instance of a type.
+   template< class Type > constexpr Size bytes_per_ = sizeof( Type );
+
     // Conversions to/from C++17 `std::byte`:
+    inline auto as_number( const std::byte value ) -> Byte;
+    inline auto as_std_byte( const Byte value ) -> std::byte;
+
+    // Documented in the <all.hpp> header.
+    namespace system
+    {
+        CPPX_USE_CPPX( Byte, Signed_byte, bits_per_byte, as_number, as_std_byte );
+    }
+
+    //------------------------------------------------------- impl
+
+    inline auto as_byte_ptr( const P_<Signed_byte> p )
+        -> P_<Byte>
+    { return reinterpret_cast<P_<Byte>>( p ); }
+
+    inline auto as_byte_ptr( const P_<const Signed_byte> p )
+        -> P_<const Byte>
+    { return reinterpret_cast<P_<const Byte>>( p ); }
+
+    inline auto as_signedbyte_ptr( const P_<Byte> p )
+        -> P_<Signed_byte>
+    { return reinterpret_cast<P_<Signed_byte>>( p ); }
+
+    inline auto as_signedbyte_ptr( const P_<const Byte> p )
+        -> P_<const Signed_byte>
+    { return reinterpret_cast<P_<const Signed_byte>>( p ); }
+
     inline auto as_number( const std::byte value )
         -> Byte
     { return std::to_integer<Byte>( value ); }
@@ -33,9 +72,4 @@ namespace cppx
         -> std::byte
     { return std::byte( value ); }
 
-    // Documented in the <all.hpp> header.
-    namespace system
-    {
-        CPPX_USE_CPPX( Byte, Signed_byte, bits_per_byte, as_number, as_std_byte );
-    }
 }  // namespace cppx
