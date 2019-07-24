bf5424b25c2defa2034216d1dae881de4989a6cf
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-07-24 11:39:39 +0200

    Added `Fixed_size_matrix_`. Refactoring for that.

--- source/cppx-core/collections/Matrix_.hpp
+++ source/cppx-core/collections/Matrix_.hpp
@@ -9,21 +9,22 @@
 
 #include <vector>       // std::vector
 #include <stdexcept>    // std::out_of_range
+#include <utility>      // std::move
 
 namespace cppx
 {
-    CPPX_USE_STD( out_of_range, vector );
+    CPPX_USE_STD( move, out_of_range, vector );
 
     namespace matrix {
-        struct Width{ Size value; };
-        struct Height{ Size value; };
-        struct Position { Size x; Size y; };
+        struct Width        { Size value; };
+        struct Height       { Size value; };
+        struct Position     { Size x; Size y; };
 
         struct Basic_types
         {
-            using Width     = matrix::Width;
-            using Height    = matrix::Height;
-            using Position  = matrix::Position;
+            using Width         = matrix::Width;
+            using Height        = matrix::Height;
+            using Position      = matrix::Position;
         };
 
         class Layout:
@@ -33,33 +34,33 @@ namespace cppx
             Size                m_height;
 
         public:
-            auto width() const  -> Size     { return m_width; }
-            auto height() const -> Size     { return m_height; }
-            auto n_items() const -> Size    { return m_width*m_height; }
+            constexpr auto width() const  -> Size   { return m_width; }
+            constexpr auto height() const -> Size   { return m_height; }
+            constexpr auto n_items() const -> Size  { return m_width*m_height; }
 
-            auto index_of( const Index col, const Index row ) const
+            constexpr auto index_of( const Index col, const Index row ) const
                 -> Index
             { return row*m_width + col; }
 
-            auto col_of( const Index i ) const
+            constexpr auto col_of( const Index i ) const
                 -> Index
             { return i % m_width; }
 
-            auto row_of( const Index i ) const
+            constexpr auto row_of( const Index i ) const
                 -> Index
             { return i / m_width; }
 
-            auto index_of_position( const Position& pos )
+            constexpr auto index_of( const Position& pos ) const
                 -> Index
             { return index_of( pos.x, pos.y ); }
 
-            auto position_of_index( const Index i )
+            constexpr auto position_of( const Index i ) const
                 -> Position
             { return Position{ col_of( i ), row_of( i ) }; }
 
-            Layout(): m_width( 0 ), m_height( 0 ) {}
+            constexpr Layout(): m_width( 0 ), m_height( 0 ) {}
 
-            Layout(
+            constexpr Layout(
                 const Width     width,
                 const Height    height
                 ):
@@ -70,11 +71,13 @@ namespace cppx
     }  // namespace matrix
 
     template< class Item_param >
-    class Matrix_:
+    class Abstract_matrix_:
         public matrix::Layout
     {
+    public:
         using Item = Item_param;
 
+    private:
         vector<Item>        m_items;
 
         template< class Self >
@@ -94,6 +97,27 @@ namespace cppx
             );
             return self.m_items[self.index_of( col, row )];
         }
+
+    protected:
+        Abstract_matrix_():
+            Layout( Width{ 0 }, Height{ 0 } ),
+            m_items()
+        {}
+
+        Abstract_matrix_(
+            const Width     width,
+            const Height    height,
+            const Item&     init_value = Item()
+            ):
+            Layout( width, height ),
+            m_items( n_items(), init_value )
+        {}
+
+        Abstract_matrix_( const Abstract_matrix_& other ) = default;
+        Abstract_matrix_( Abstract_matrix_&& other ) = default;
+
+        auto operator=( const Abstract_matrix_& other ) -> Abstract_matrix_& = default;
+        auto operator=( Abstract_matrix_&& other ) -> Abstract_matrix_& = default;
 
     public:
         auto begin() const      { return m_items.begin(); }
@@ -118,60 +142,134 @@ namespace cppx
         auto at( const Index col, const Index row )
             -> Item&
         { return impl_item_at_checked( col, row, *this );  }
-
-        Matrix_():
-            Layout( Width{ 0 }, Height{ 0 } ),
-            m_items()
-        {}
-
-        Matrix_(
-            const Width     width,
-            const Height    height,
-            const Item&     init_value = Item()
-            ):
-            Layout( width, height ),
-            m_items( n_items(), init_value )
-        {}
     };
 
     template< class Item >
-    inline auto item_at( const Index col, const Index row, const Matrix_<Item>& m )
-        -> const Item&
+    inline auto item_at(
+        const Index col, const Index row, const Abstract_matrix_<Item>& m
+        ) -> const Item&
     { return m( col, row ); }
 
     template< class Item >
-    inline auto item_at( const Index col, const Index row, Matrix_<Item>& m )
-        -> Item&
+    inline auto item_at(
+        const Index col, const Index row, Abstract_matrix_<Item>& m
+        ) -> Item&
     { return m( col, row ); }
 
     template< class Item >
-    inline auto item_at_checked( const Index col, const Index row, const Matrix_<Item>& m )
-        -> const Item&
+    inline auto item_at_checked(
+        const Index col, const Index row, const Abstract_matrix_<Item>& m
+        ) -> const Item&
     { return m.at( col, row ); }
 
     template< class Item >
-    inline auto item_at_checked( const Index col, const Index row, Matrix_<Item>& m )
-        -> Item&
+    inline auto item_at_checked(
+        const Index col, const Index row, Abstract_matrix_<Item>& m
+        ) -> Item&
     { return m.at( col, row ); }
 
     template< class Item>
-    inline auto item_at( const matrix::Position& pos, const Matrix_<Item>& m )
-        -> const Item&
+    inline auto item_at(
+        const matrix::Position& pos, const Abstract_matrix_<Item>& m
+        ) -> const Item&
     { return item_at( pos.x, pos.y, m ); }
 
     template< class Item>
-    inline auto item_at( const matrix::Position& pos, Matrix_<Item>& m )
-        -> Item&
+    inline auto item_at(
+        const matrix::Position& pos, Abstract_matrix_<Item>& m
+        ) -> Item&
     { return item_at( pos.x, pos.y, m ); }
 
     template< class Item>
-    inline auto item_at_checked( const matrix::Position& pos, const Matrix_<Item>& m )
-        -> const Item&
+    inline auto item_at_checked(
+        const matrix::Position& pos, const Abstract_matrix_<Item>& m
+        ) -> const Item&
     { return item_at_checked( pos.x, pos.y, m ); }
 
     template< class Item>
-    inline auto item_at_checked( const matrix::Position& pos, Matrix_<Item>& m )
-        -> Item&
+    inline auto item_at_checked(
+        const matrix::Position& pos, Abstract_matrix_<Item>& m
+        ) -> Item&
     { return item_at_checked( pos.x, pos.y, m ); }
+
+    template< class Item_param, Size w, Size h >
+    class Fixed_size_matrix_:
+        public Abstract_matrix_<Item_param>
+    {
+        using AM_base = Abstract_matrix_<Item_param>;
+
+    public:
+        using Item = typename AM_base::Item;    // Alias Item_param.
+
+        static constexpr matrix::Layout layout = matrix::Layout(
+            matrix::Width{ w },
+            matrix::Height{ h }
+        );
+
+        auto operator=( const Fixed_size_matrix_& other )
+            -> Fixed_size_matrix_&
+        {
+            static_cast<AM_base&>( *this ) = other;
+            return *this;
+        }
+
+        auto operator=( Fixed_size_matrix_&& other )
+            -> Fixed_size_matrix_&
+        {
+            static_cast<AM_base&>( *this ) = move( other );
+            return *this;
+        }
+
+        Fixed_size_matrix_( const Item& init_value = Item() ):
+            AM_base( matrix::Width{ w }, matrix::Height{ h }, init_value )
+        {}
+
+        Fixed_size_matrix_( const Fixed_size_matrix_& other ):
+            AM_base( other )
+        {}
+
+        Fixed_size_matrix_( Fixed_size_matrix_&& other ):
+            AM_base( move( other ) )
+        {}
+    };
+
+    template< class Item_param >
+    class Matrix_:
+        public Abstract_matrix_<Item_param>
+    {
+        using AM_base = Abstract_matrix_<Item_param>;
+
+    public:
+        using Item = typename AM_base::Item;    // Alias Item_param.
+
+        using AM_base::Abstract_matrix_;        // Constructors, including copy & move.
+        using AM_base::operator=;               // Copy and move assignment operators.
+
+        template< Size w, Size h >
+        auto operator=( const Fixed_size_matrix_<Item, w, h>& other )
+            -> Matrix_&
+        {
+            static_cast<AM_base&>( *this ) = other;
+            return *this;
+        }
+
+        template< Size w, Size h >
+        auto operator=( Fixed_size_matrix_<Item, w, h>&& other )
+            -> Matrix_&
+        {
+            static_cast<AM_base&>( *this ) = move( other );
+            return *this;
+        }
+
+        template< Size w, Size h >
+        Matrix_( const Fixed_size_matrix_<Item, w, h>& other ):
+            AM_base( other )
+        {}
+
+        template< Size w, Size h >
+        Matrix_( Fixed_size_matrix_<Item, w, h>&& other ):
+            AM_base( move( other ) )
+        {}
+    };
 
 }  // namespace cppx
