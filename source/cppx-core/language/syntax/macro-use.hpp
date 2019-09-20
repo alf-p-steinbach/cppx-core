60b5507df4de0908fb07ea7681125ae07e25cfe1
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-09-20 08:37:13 +0200

    Update macro-use.hpp

--- source/cppx-core/language/syntax/macro-use.hpp
+++ source/cppx-core/language/syntax/macro-use.hpp
@@ -18,13 +18,22 @@
 #   define $use_from_namespace          CPPX_USE_FROM_NAMESPACE         ///< \dollarname{use_from_namespace}
 #   define $use_cppx                    CPPX_USE_CPPX                   ///< \dollarname{use_cppx}
 #   define $use_std                     CPPX_USE_STD                    ///< \dollarname{use_std}
-#   define $use_nsname_in               CPPX_USE_NSNAME_IN              ///< \dollarname{use_nsname_in}
+#   define $use_nsname_from             CPPX_USE_NSNAME_FROM            ///< \dollarname{use_nsname_from}
 #   define $use_cppx_nsname             CPPX_USE_CPPX_NSNAME            ///< \dollarname{use_cppx_nsname}
 #   define $use_std_nsname              CPPX_USE_STD_NSNAME             ///< \dollarname{use_std_nsname}
-#   define $use_nsnames_in              CPPX_USE_NSNAMES_IN             ///< \dollarname{use_nsnames_in}
+#   define $use_nsnames_from            CPPX_USE_NSNAMES_FROM           ///< \dollarname{use_nsnames_from}
 #   define $use_cppx_nsnames            CPPX_USE_CPPX_NSNAMES           ///< \dollarname{use_cppx_nsnames}
 #   define $use_std_nsnames             CPPX_USE_STD_NSNAMES            ///< \dollarname{use_std_nsnames}
+
+// Old names for compatibility.
+#   define $use_nsname_in               CPPX_USE_NSNAME_FROM
+#   define $use_nsnames_in              CPPX_USE_NSNAMES_FROM
 #endif
+
+// Old names for compatibility.
+#define CPPX_USE_NSNAME_IN              CPPX_USE_NSNAME_FROM
+#define CPPX_USE_NSNAMES_IN             CPPX_USE_NSNAMES_FROM
+
 
 /// \hideinitializer
 /// \brief Use each specified item `ITEM` from namespace `NS` as if by `using NS::ITEM;`.
@@ -56,27 +65,27 @@
 ///
 /// \param  parent_ns   The namespace containing the relevant nested namespace.
 /// \param  nested_ns   The nested namespace.
-#define CPPX_USE_NSNAME_IN( parent_ns, nested_ns ) \
+#define CPPX_USE_NSNAME_FROM( parent_ns, nested_ns ) \
     namespace nested_ns = parent_ns::nested_ns
 
 /// \brief Use `NESTED_NS` as an alias for namespace `cppx::NESTED_NS`.
 ///
 /// \param  nested_ns   The nested namespace.
 #define CPPX_USE_CPPX_NSNAME( nested_ns ) \
-    CPPX_USE_NSNAME_IN( cppx, nested_ns )
+    CPPX_USE_NSNAME_FROM( cppx, nested_ns )
 
 /// \brief Use `NESTED_NS` as an alias for namespace `std::NESTED_NS`.
 ///
 /// \param  nested_ns   The nested namespace.
 #define CPPX_USE_STD_NSNAME( nested_ns ) \
-    CPPX_USE_NSNAME_IN( std, nested_ns )
+    CPPX_USE_NSNAME_FROM( std, nested_ns )
 
 /// \hideinitializer
 /// \brief Use each specified `NESTED_NS` as an alias for namespace `PARENT_NS::NESTED_NS`.
 ///
 /// \param  parent_ns   The namespace containing the relevant nested namespace.
 /// \param  ...         A comma separated list of names of nested namespaces.
-#define CPPX_USE_NSNAMES_IN( parent_ns, ... ) \
+#define CPPX_USE_NSNAMES_FROM( parent_ns, ... ) \
     CPPX_APPLY_WITH_FIXED_ARG( CPPX_GENERATE_NSNAME_USAGE, parent_ns, __VA_ARGS__ ) \
     static_assert( !!"Terminate this statement with a semicolon" )
 
@@ -89,10 +98,10 @@
 ///
 /// \param  ...         A comma separated list of names of nested namespaces.
 #define CPPX_USE_CPPX_NSNAMES( ... ) \
-    CPPX_USE_NSNAMES_IN( cppx, __VA_ARGS__ )
+    CPPX_USE_NSNAMES_FROM( cppx, __VA_ARGS__ )
 
 /// \brief Use each specified `NESTED_NS` as an alias for namespace `std::NESTED_NS`.
 ///
 /// \param  ...         A comma separated list of names of nested namespaces.
 #define CPPX_USE_STD_NSNAMES( ... ) \
-    CPPX_USE_NSNAMES_IN( std, __VA_ARGS__ )
+    CPPX_USE_NSNAMES_FROM( std, __VA_ARGS__ )
