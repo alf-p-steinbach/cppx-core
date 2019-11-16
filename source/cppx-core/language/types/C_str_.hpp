c8c4f9fdb1897dd4996e833a338bfc7664e1f9ab
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-11-16 19:16:59 +0100

    Update C_str_.hpp

--- source/cppx-core/language/types/C_str_.hpp
+++ source/cppx-core/language/types/C_str_.hpp
@@ -24,16 +24,16 @@
 ///
 ///     constexpr string_view s = "Blah";
 
-#include <cppx-core/language/types/type-makers.hpp>     // cppx::(P_, Const_, Unconst_)
+#include <cppx-core/language/types/type-makers.hpp>     // cppx::(Type_, Const_, Unconst_)
 
 namespace cppx
 {
     // Enable_if_<is_a_cpp03_char_type_<Unconst_<Char>>>
     template< class Char >
-    using C_str_            = P_<Const_<Char>>;     // Effectively `Char const*`.
+    using C_str_            = Type_<Const_<Char>*>;
 
     template< class Char >
-    using Mutable_c_str_    = P_<Unconst_<Char>>;   // Effectively `Unconst_<Char>*`.
+    using Mutable_c_str_    = Type_<Unconst_<Char>*>;
 
     using C_str                 = C_str_<char>;             // Effectively `char const*`.
     using Wide_c_str            = C_str_<wchar_t>;          // Effectively `wchar_t const*`.
