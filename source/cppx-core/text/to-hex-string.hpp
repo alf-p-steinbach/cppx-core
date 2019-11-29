01adcb8071c33d07867ca620e4883692f5683ff1
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-11-29 10:22:40 +0100

    Moved remove_leading_zeroes_in.hpp to cppx-core-language project

--- source/cppx-core/text/to-hex-string.hpp
+++ source/cppx-core/text/to-hex-string.hpp
@@ -1,12 +1,12 @@
 ﻿#pragma once    // Source encoding: UTF-8 with BOM (π is a lowercase Greek "pi").
 
-#include <cppx-core/collections/Value_bytes_.hpp>               // cppx::Value_bytes_
-#include <cppx-core-language/tmp/Enable_if_.hpp>                // cppx::Enable_if_
-#include <cppx-core-language/tmp/type-traits.hpp>               // cppx::(is_xxx_)
-#include <cppx-core-language/syntax/macro-use.hpp>              // CPPX_USE_STD
-#include <cppx-core/text/remove_leading_zeroes_in.hpp>          // cppx::remove_leading_zeroes_in
+#include <cppx-core/collections/Value_bytes_.hpp>                   // cppx::Value_bytes_
 
-#include <cppx-core-language/ascii/to-hex-in-buffer.hpp>        // cppx::*
+#include <cppx-core-language/ascii/remove_leading_zeroes_in.hpp>    // cppx::remove_leading_zeroes_in
+#include <cppx-core-language/ascii/to-hex-in-buffer.hpp>            // cppx::*
+#include <cppx-core-language/syntax/macro-use.hpp>                  // CPPX_USE_STD
+#include <cppx-core-language/tmp/Enable_if_.hpp>                    // cppx::Enable_if_
+#include <cppx-core-language/tmp/type-traits.hpp>                   // cppx::(is_xxx_)
 
 #include <c/assert.hpp>     // assert
 #include <string>           // std::string
--- source/dummy-header-includes/text/remove_leading_zeroes_in.include.cpp
+++
@@ -1,2 +0,0 @@
-#include <cppx-core/text/remove_leading_zeroes_in.hpp>
-namespace { int satisfy_linker; }
