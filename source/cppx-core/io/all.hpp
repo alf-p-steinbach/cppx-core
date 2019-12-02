74b13fcf30fa797846dfa70fa5bafa86a1cd34ff
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-12-02 04:33:57 +0100

    Cleanup

--- source/cppx-core/io/all.hpp
+++ source/cppx-core/io/all.hpp
@@ -3,4 +3,3 @@
 #include <cppx-core/io/C_file.hpp>
 #include <cppx-core/io/debugging-output.hpp>
 #include <cppx-core/io/monospaced_bullet_block.hpp>
-#include <cppx-core/io/Type_name_of_.output.hpp>
--- source/dummy-header-includes/io/type_name_of.output.include.cpp
+++
@@ -1,2 +0,0 @@
-#include <cppx-core/io/type_name_of_.output.hpp>
-namespace{ int satisfy_linker; }
