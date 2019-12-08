e49dff89eed4055a083579e7daec4df1e6b56405
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2019-12-08 11:50:19 +0100

    Moved Forward_iterator_impl_ to cppx-core-language project

--- source/cppx-core/collections/Enumerated_.hpp
+++ source/cppx-core/collections/Enumerated_.hpp
@@ -1,9 +1,9 @@
 ﻿#pragma once    // Source encoding: UTF-8 with BOM (π is a lowercase Greek "pi").
 
 #include <cppx-core/collections/dynamic-size-checking.hpp>          // cppx::n_items_of
-#include <cppx-core/iterators/Forward_iterator_impl_.hpp>           // cppx::Forward_iterator_impl_
 #include <cppx-core-language/syntax/type-builders.hpp>              // cppx::P_
 #include <cppx-core-language/syntax/macro-define_tag.hpp>           // CPPX_DEFINE_TAG
+#include <cppx-core-language/types/Forward_iterator_impl_.hpp>      // cppx::Forward_iterator_impl_
 #include <cppx-core-language/types/Truth.hpp>                       // cppx::Truth
 #include <cppx-core/meta-type/collection-traits.hpp>                // cppx::Iterator_for_
 
--- source/cppx-core/iterators/all.hpp
+++ source/cppx-core/iterators/all.hpp
@@ -1,4 +1,3 @@
 ﻿#pragma once    // Source encoding: UTF-8 with BOM (π is a lowercase Greek "pi").
 
 #include <cppx-core/iterators/Count_iterator_.hpp>
-#include <cppx-core/iterators/Forward_iterator_impl_.hpp>
